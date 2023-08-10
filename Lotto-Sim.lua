41ede3a1a0076e36d76e787654d86618eba49fc9
KaikuGz <141708787+KaikuGz@users.noreply.github.com>
2023-08-10 13:24:47 +0300

    Update Lotto-Sim.lua

--- Lotto-Sim.lua
+++ Lotto-Sim.lua
@@ -1,9 +1,13 @@
+-- Writen By Kaiku
+-- Lotto Sim in Lua
+
 Oikeat_Numerot = {}
 Arvatut_Numerot = {}
 
 MaxLuku = 42
 Maara = 7
 
+-- GENEROI VOITTONUMEROT
 while #Oikeat_Numerot < Maara do
     local r = math.random(0, MaxLuku)
     local isDuplicate = false
@@ -13,20 +17,30 @@ while #Oikeat_Numerot < Maara do
             break
         end
     end
+    
+    --JOS EI OLE JO LISTASSA NIIN LISÄÄ
     if not isDuplicate then
         table.insert(Oikeat_Numerot, r)
     end
 end
 
+
 print("GIVE UR NUMBERS")
+--KYSYY PELAAJAN NUMEROT YKSITELLEN
+
 while #Arvatut_Numerot < Maara do
     local s = io.read("*n")
     local isDuplicate = false
     local isTooLarge = false
-    if s > MaxLuku then
+    
+    -- KATSOO ONKO NUMERO YLI MAX LUVUN
+    if s > MaxLuku then        
         print("TOO LARGE NUMBER")
         isTooLarge = true
     else
+
+        -- JOS EI OLE YLI MAX LUVUN KATSOO ONKO JO LISTASSA
+        
         for _, value in ipairs(Arvatut_Numerot) do
             if value == s then
                 isDuplicate = true
@@ -35,12 +49,14 @@ while #Arvatut_Numerot < Maara do
             end
         end
     end
+
+    -- LISÄÄ JOS EI OLE LISTASSA
     if not isDuplicate and not isTooLarge then
         table.insert(Arvatut_Numerot, s)        
     end
 end
 
-
+-- LASKEE KUINKA MONTA OIKEAA NUMEROA LÖYTYI
 local laskin = 0
 for _, guessedValue in ipairs(Arvatut_Numerot) do
     for _, correctValue in ipairs(Oikeat_Numerot) do
@@ -50,6 +66,7 @@ for _, guessedValue in ipairs(Arvatut_Numerot) do
     end
 end
 
+-- PRINTTAA VIELLÄ OMAT NUMEROT JA VOITTO NUMEROT
 print("[ ARVATUT ]")
 for index, value in ipairs(Arvatut_Numerot) do
     print("[",Arvatut_Numerot[index],"]")
