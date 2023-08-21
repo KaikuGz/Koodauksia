e6559892290dec21db9809ae1d3b6bd90f447504
KaikuGz <141708787+KaikuGz@users.noreply.github.com>
2023-08-21 10:34:00 +0300

    Add files via upload

---
+++ gridMap-gen.cs
@@ -0,0 +1,53 @@
+﻿// WRITEN BY KADDI
+// SIMPLE GRID MAP "GENERATOR"
+
+List<string> Lista = new List<string>();
+int maara = 2000;
+int printaus = 50;
+
+for (int i = 0; i < maara; i++)
+{
+    Lista.Add("[ ]");
+}
+
+void Printaus()
+{
+    int laskin = 0;
+    for (int i = 0; i < maara; i++)
+    {
+        if(laskin != printaus)
+        {
+            Console.Write(Lista[i]);
+            laskin++;
+        }
+        if (laskin == printaus)
+        {
+            laskin = 0;
+            Console.WriteLine();
+        }
+    }
+    for (int i = 0; i < maara; i++)
+    {
+        Lista[i] = "[ ]";
+    }
+}
+
+void RamdomGeneraatio()
+{
+    var x = Console.ReadLine();
+    var random = new Random();
+
+    for (int i = 0; i < maara; i++)
+    {
+        if (random.Next(0, 3) == 1)
+        {
+            Lista[i] = "[X]";
+        }
+    }
+}
+
+while(true)
+{
+    RamdomGeneraatio();
+    Printaus();
+}
